a88612c3eebb410215b31b797a8655b856a52f1c
ynavrom <62753738+ynavrom@users.noreply.github.com>
2020-04-10 13:26:18 +0200

    Add files via upload

---
+++ Bluetooth/capteur_infrarouge.h
@@ -0,0 +1,15 @@
+/*
+ * capteur_infrarouge.h
+ *
+ *  Created on: 8 avr. 2020
+ *      Author: MORVA
+ */
+
+#ifndef CAPTEUR_INFRAROUGE_H_
+#define CAPTEUR_INFRAROUGE_H_
+
+
+int capteur_infrarouge(void);
+
+
+#endif /* CAPTEUR_INFRAROUGE_H_ */
